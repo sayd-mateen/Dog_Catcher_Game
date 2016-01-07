7d79f28678c64002651b18dce2c41bf4e7286432
sayd-mateen <sayed_mateen55@yahoo.com>
2016-01-06 20:51:14 -0800

    Create Catcher.java

---
+++ a1/GameObject/Catcher.java
@@ -0,0 +1,21 @@
+/**
+ * Name: Sayd Mateen
+ * Class: CSC133
+ * Assignment#1 
+ * Date: 10/02/15
+ *  
+ *
+ * This class will be more implemented later when we add graphic interface.
+ * For now there it just implents the inface and connects the gameObject with the Net class.
+ * 
+ * 
+ * 
+ */
+
+package a1.gameObject;
+
+import a1.IGuided;
+
+public abstract class Catcher extends GameObject implements IGuided{
+	
+}
