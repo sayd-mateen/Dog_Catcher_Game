0cbd6b5c899a02f99c4a1df6ffd57482d412262b
sayd-mateen <sayed_mateen55@yahoo.com>
2016-01-06 20:26:28 -0800

    Create Starter.java

---
+++ Starter.java
@@ -0,0 +1,21 @@
+/**
+ * Name: Sayd Mateen
+ * Class: CSC133
+ * Assignment#1 
+ * Date: 10/02/15
+ * 
+ * Creates a Game class which will start and run the Dog Catcher. 
+ *
+ * 
+ */
+
+
+package a1;
+
+public class Starter {
+	// Main will be the starter of the game. Creates the Game instance which will run the program. 
+	public static void main(String[] args) {		
+		@SuppressWarnings("unused")
+		Game g = new Game();
+	}
+}
